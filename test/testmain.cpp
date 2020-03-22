d4835c28bb5e7e72a15af408cb1a667f4a4f19b1
jackschu <js9pa@virginia.edu>
2020-03-21 23:30:00 -0400

    adding a test

--- test/testmain.cpp
+++ test/testmain.cpp
@@ -1,1 +1,23 @@
-int main(){}
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "board.h"
+#include "tile.h"
+
+TEST_CASE("diagonal win", "[win check]"){
+  Board b = Board();
+  b.makeMove(3, Tile::BLACK); 
+  b.makeMove(4, Tile::WHITE);
+  b.makeMove(5, Tile::WHITE);
+  b.makeMove(6, Tile::WHITE);
+
+  b.makeMove(4, Tile::BLACK);
+  b.makeMove(5, Tile::WHITE);
+  b.makeMove(6, Tile::WHITE);
+  
+  b.makeMove(5, Tile::BLACK);
+  b.makeMove(6, Tile::WHITE);
+  
+  b.makeMove(5, Tile::BLACK);
+  REQUIRE(b.checkWin(Tile::WHITE) == false);
+  REQUIRE(b.checkWin(Tile::BLACK) == false);
+}
